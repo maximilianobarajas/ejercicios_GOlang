bc2155078568406d68ac5426dafd6581fd702bd3
Maximiliano Barajas Sánchez <111471026+maximilianobarajas@users.noreply.github.com>
2023-01-15 13:17:40 -0600

    Create estructura_persona.go

---
+++ estructura_persona.go
@@ -0,0 +1,31 @@
+package main
+  
+import (
+    "fmt"
+)
+  
+//Declaramos la estructura
+type Estudiante struct{
+      
+    // definimos los atributos
+    nombre string
+    apellido string
+    edad int
+    carrera string
+}
+  
+//Funcion de descripción
+func (estudiante Estudiante) imprimir_detalles(){
+  
+    fmt.Printf("Nombre completo: %s %s", estudiante.nombre, estudiante.apellido)
+    fmt.Printf("\nCarrera: %s",estudiante.carrera)
+    fmt.Printf("\nEdad: %d \n", estudiante.edad)
+}
+
+func main() {
+    //Instanciamos la estructura
+    estudiante1 := Estudiante{"Alan", "Perez", 22,"Lic. en Derecho"}
+      
+    //Imprimimos la estructura
+    estudiante1.imprimir_detalles()
+}
